8eb018de85e91ddd2f84087e97b326785bdbd054
cyttic <cyttic@gmail.com>
2025-06-23 15:45:45 +0300

    pattern proxy

--- src/delegator/BusinessDelegatePatternDemo.java
+++ src/delegator/BusinessDelegatePatternDemo.java
@@ -4,10 +4,11 @@ public class BusinessDelegatePatternDemo {
 
     public static void main(String[] args) {
 
+        /*
         ClassA c3 = new ClassC();
         c3.funcA();
         System.out.println(c3);
-
+        */
 
         BusinessDelegate businessDelegate = new BusinessDelegate();
         businessDelegate.setServiceType("EJB");
